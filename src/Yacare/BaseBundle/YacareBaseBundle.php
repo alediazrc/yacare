532644a1d53bde12df4ca92cc1dca90d89a77d2c
Ernesto Carrea <equistango@gmail.com>
2012-12-20 19:24:22 +0000

    Importación inicial.

---
+++ src/Yacare/BaseBundle/YacareBaseBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Yacare\BaseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YacareBaseBundle extends Bundle
+{
+}
---
+++ src/Yacare/InicioBundle/YacareInicioBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Yacare\InicioBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YacareInicioBundle extends Bundle
+{
+}
