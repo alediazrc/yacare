ea0f57e6e76354c981bfecf248c09590230569aa
Miguel Martinez <mmartinez@riogrande.gob.ar>
2012-12-21 17:39:43 +0000

    Se creó Dependencias, Edificio y Oficina

---
+++ src/Yacare/BaseBundle/Entity/Dependencia.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Yacare\BaseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Yacare\BaseBundle\Entity\Dependencia
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Dependencia
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $Nombre
+     *
+     * @ORM\Column(name="Nombre", type="string", length=255)
+     */
+    private $Nombre;
+
+    /**
+     * @var integer $Parent
+     *
+     * @ORM\Column(name="Parent", type="integer")
+     */
+    private $Parent;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set Nombre
+     *
+     * @param string $nombre
+     * @return Dependencia
+     */
+    public function setNombre($nombre)
+    {
+        $this->Nombre = $nombre;
+    
+        return $this;
+    }
+
+    /**
+     * Get Nombre
+     *
+     * @return string 
+     */
+    public function getNombre()
+    {
+        return $this->Nombre;
+    }
+
+    /**
+     * Set Parent
+     *
+     * @param integer $parent
+     * @return Dependencia
+     */
+    public function setParent($parent)
+    {
+        $this->Parent = $parent;
+    
+        return $this;
+    }
+
+    /**
+     * Get Parent
+     *
+     * @return integer 
+     */
+    public function getParent()
+    {
+        return $this->Parent;
+    }
+}
