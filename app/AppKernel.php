ff6830955163a0d4f8a4116b52bae948695b28e3
Ezequiel Riquelme <rezequiel.tdf@gmail.com>
2014-03-25 15:04:19 +0000

    Former-commit-id: 1ad2cdef348b1a1db0a192b4dafb4a83111d8262

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             new Yacare\ZoonosisBundle\YacareZoonosisBundle(),
             new Yacare\MunirgBundle\YacareMunirgBundle(),
             new Yacare\ExpedientesBundle\YacareExpedientesBundle(),
+            new Yacare\ObrasParticularesBundle\YacareObrasParticularesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
