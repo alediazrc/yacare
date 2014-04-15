2156d27dfd42e4598b3413d39b37b6807b48446f
Ernesto Carrea <equistango@gmail.com>
2014-04-15 19:42:16 +0000

    Former-commit-id: 2426028a564a5a5335c2083ce3d0ac7b3e5abd30

---
+++ docs/Yacare.mm
@@ -0,0 +1,348 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1397499799371" ID="ID_1330688129" MODIFIED="1397585278230" TEXT="Yacar&#xe9;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Descripci&#243;n: Aplicaci&#243;n para la gesti&#243;n integral del municipio.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Objetivo: Asistir a los agentes para mejorar el resultado de la gesti&#243;n en calidad y eficiencia. Aportar mejoras en la calidad mediante la medici&#243;n y mejora del servicio; y mejoras en la eficiencia mediante la agilizaci&#243;n y simplificaci&#243;n de los tr&#225;mites.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1397499852375" HGAP="49" ID="ID_1892952041" MODIFIED="1397584342297" POSITION="right" TEXT="Desarrollo" VSHIFT="-69">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      El desarrollo se realiza en leguaje PHP, utilizando las siguientes tecnolog&#237;as:
+    </p>
+    <ul>
+      <li>
+        Symfony 2 (MVC)
+      </li>
+      <li>
+        Bootstrap (HTML5 + CSS3)
+      </li>
+      <li>
+        jQuery (JavaScript)
+      </li>
+      <li>
+        MySQL/MariaDB (SQL)
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1397504917005" HGAP="42" ID="ID_1674749588" MODIFIED="1397585163671" TEXT="Sistemas heredados" VSHIFT="-38">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Relevamiento de los sistemas actualmente presentes en el municipio, que deber&#237;an ser reemplazados por m&#243;dulos de Yacar&#233;.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1397504932949" ID="ID_899229907" MODIFIED="1397584189148" TEXT="Comercio">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1397583525927" ID="ID_1985765570" MODIFIED="1397584461050" TEXT="Habilitaciones comerciales">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        Gesti&#243;n de la base de datos de comercios habilitados
+      </li>
+      <li>
+        Emisi&#243;n de certificados de habilitaci&#243;n
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1397504963613" ID="ID_555568978" MODIFIED="1397584189148" TEXT="Obras particulaes">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1397504970372" ID="ID_112516630" MODIFIED="1397584189150" TEXT="Inspecciones comercio">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node COLOR="#111111" CREATED="1397505032780" ID="ID_1863570778" MODIFIED="1397584553302" TEXT="OPart">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Uso: Obras Particulares
+      </li>
+      <li>
+        Ubicaci&#243;n: \\archivos\oparticulares\oparticulares\rop271204\opart.exe
+      </li>
+      <li>
+        Lenguaje: Visual Basic 6
+      </li>
+      <li>
+        LOC: 10000
+      </li>
+      <li>
+        Desarrollador: ?
+      </li>
+      <li>
+        Fuentes: si
+      </li>
+      <li>
+        Datos: Archivo MDB con ruta hardcodeada en \\archivos\oparticulares\oparticulares\bases\Obras Particulares.mdb
+      </li>
+      <li>
+        Instalador: no
+      </li>
+      <li>
+        Notas:
+      </li>
+      <li>
+        Utiliza la funci&#243;n Val() y hace conversiones impl&#237;citas de double a string, lo que causa problemas con el separador de decimales. Hay que cambiar la configuraci&#243;n regional para que utilice punto como separador de decimales y coma como separador de miles.
+      </li>
+      <li>
+        Imprime comprobantes sencillos para el contribuyente
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+</node>
+<node COLOR="#111111" CREATED="1397505037044" ID="ID_48829863" MODIFIED="1397584189149" TEXT="MovPrevias">
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1397505340817" ID="ID_1676725565" MODIFIED="1397584724020" TEXT="Recursos humanos">
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1397505053675" ID="ID_815039602" MODIFIED="1397584189149" TEXT="Parte">
+<edge STYLE="sharp_bezier"/>
+</node>
+<node COLOR="#111111" CREATED="1397505046972" ID="ID_501658133" MODIFIED="1397584798090" TEXT="Gestion">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Uso: ?
+      </li>
+      <li>
+        Ubicaci&#243;n: \\archivos\Gestion2\gestion.exe
+      </li>
+      <li>
+        Lenguaje: FoxPro?
+      </li>
+      <li>
+        LOC: ?
+      </li>
+      <li>
+        Desarrollador:
+      </li>
+      <li>
+        Fuentes: ?
+      </li>
+      <li>
+        Datos: MySQL, host 192.168.100.5, base &#8220;rr_hh&#8221;
+      </li>
+      <li>
+        Instalador: ?
+      </li>
+      <li>
+        Notas:
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1397584554211" ID="ID_1397512216" MODIFIED="1397584564277" TEXT="Catastro">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1397584559651" HGAP="62" ID="ID_554931812" MODIFIED="1397584606067" TEXT="Puertas" VSHIFT="17">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Uso: Catastro / N&#250;meros de puerta
+      </li>
+      <li>
+        Ubicaci&#243;n: http://antares/puertas
+      </li>
+      <li>
+        Lenguaje: PHP
+      </li>
+      <li>
+        LOC: 5000
+      </li>
+      <li>
+        Desarrollador:
+      </li>
+      <li>
+        Fuentes: si
+      </li>
+      <li>
+        Datos: Oracle, host 192.168.100.20, SID dbmunirg
+      </li>
+      <li>
+        Instalador: n/a
+      </li>
+      <li>
+        Notas:
+      </li>
+      <li>
+        Emite archivos PDF (usando fpdf) para impresi&#243;n de comprobantes sencillos para el contribuyente.
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1397584606778" ID="ID_812258994" MODIFIED="1397584620243" TEXT="Deportes">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1397584621497" ID="ID_1911641853" MODIFIED="1397584666310" TEXT="Natatorio">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Uso: Natatorio
+      </li>
+      <li>
+        Ubicaci&#243;n: http://webmuni/natatorio
+      </li>
+      <li>
+        Lenguaje: PHP
+      </li>
+      <li>
+        LOC: 8000
+      </li>
+      <li>
+        Desarrollador: Heber Villegas
+      </li>
+      <li>
+        Fuentes: si
+      </li>
+      <li>
+        Datos: MySQL, host 192.168.100.5, base &#8220;natatorio_real&#8221;
+      </li>
+      <li>
+        Instalador: n/a
+      </li>
+      <li>
+        Tiene algunas clases que simulan un ORM muy b&#225;sico.
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1397584666905" ID="ID_1575110434" MODIFIED="1397584704723" TEXT="Tr&#xe1;nsito">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1397584681593" ID="ID_817932790" MODIFIED="1397584722911" TEXT="Tr&#xe1;nsito">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Uso: ?
+      </li>
+      <li>
+        Ubicaci&#243;n: http://webmuni/transitosp
+      </li>
+      <li>
+        Lenguaje: PHP
+      </li>
+      <li>
+        LOC: 20000
+      </li>
+      <li>
+        Desarrollador:
+      </li>
+      <li>
+        Fuentes: s&#237;
+      </li>
+      <li>
+        Datos: MySQL, host 192.168.100.5, base &#8220;serv_public_transp&#8221;
+      </li>
+      <li>
+        Instalador: n/a
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1397505367920" HGAP="45" ID="ID_191676764" MODIFIED="1397585114051" TEXT="Modulos nuevos" VSHIFT="-2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1397505374394" ID="ID_846759959" MODIFIED="1397585158195" TEXT="Tramites">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Estado: beta
+    </p>
+    <p>
+      Reemplaza a: ninguno
+    </p>
+    <p>
+      Descripcion: gestion de tramites en general
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
