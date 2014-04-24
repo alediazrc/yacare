02a2438c3e2f070178833bfb940f740bd937bf04
Miguel Martinez <mmartinez@riogrande.gob.ar>
2014-04-24 18:01:32 +0000

    Nueva puesta en marcha
    Former-commit-id: c9094e9139f7aa4f68dd978693e91c3fa43d8f5f

--- web/mobil/inspeccion/sinc.php
+++ web/mobil/inspeccion/sinc.php
@@ -1,105 +1,103 @@
 <?php
-	include_once 'global.php.inc';
-	include_once 'db_local.php.inc';
-	include_once 'db_remota.php.inc';
+include_once 'global.php.inc';
+include_once 'db_local.php.inc';
+include_once 'db_remota.php.inc';
 ?>
 
 <body>
-<div class="encab">
-<div class="encab-izquierda"><img src="yacare_logo_48bw.png" width="48px">&nbsp;Yacaré - Inspección</div>
-<div class="encab-derecha">
- <button onclick="parent.location='listado.php'">Volver</a>
-</div>
-</div>
+    <div class="encab">
+        <div class="encab-izquierda"><img src="yacare_logo_48bw.png" width="48px">&nbsp;Yacaré - Inspección</div>
+        <div class="encab-derecha">
+            <button onclick="parent.location = 'listado.php'">Volver</button>
+        </div>
+    </div>
 
-<div class="contenido">
-<h1>Resultados de la sincronización</h1>
-<?php
-    $SqliteOrigen = "inspeccion.sqlite";
-    $SqliteDestino = "inspeccion-backup-" . date('Ymdhis') . ".sqlite";
-    copy($SqliteOrigen, $SqliteDestino);
+    <div class="contenido">
+        <h1>Resultados de la sincronización</h1>
+        <?php
+        $SqliteOrigen = "inspeccion.sqlite";
+        $SqliteDestino = "inspeccion-backup-" . date('Ymdhis') . ".sqlite";
+        copy($SqliteOrigen, $SqliteDestino);
 
-	$IdEncargadoDispositivo = $db_remota->query("SELECT Encargado_id FROM Base_Dispositivo WHERE IdentificadorUnico='$mac'")->fetchColumn();
-	$IdRelevamientoActual = $db_remota->query("SELECT MAX(id) FROM Inspeccion_Relevamiento")->fetchColumn();
+        $IdEncargadoDispositivo = $db_remota->query("SELECT Encargado_id FROM Base_Dispositivo WHERE IdentificadorUnico='$mac'")->fetchColumn();
+        $IdRelevamientoActual = $db_remota->query("SELECT MAX(id) FROM Inspeccion_Relevamiento")->fetchColumn();
 
-	// ********************** Traigo tipos de Incidentes
-	echo "<p>Recibiendo tipos de incidente: ";
-	$db_local->exec("DROP TABLE Inspeccion_RelevamientoResultado;");
-	$db_local->exec("CREATE TABLE Inspeccion_RelevamientoResultado (Id INTEGER PRIMARY KEY, Nombre, Grupo);");
+        // ********************** Traigo tipos de Incidentes
+        echo "<p>Recibiendo tipos de incidente: ";
+        $db_local->exec("DROP TABLE Inspeccion_RelevamientoResultado;");
+        $db_local->exec("CREATE TABLE Inspeccion_RelevamientoResultado (Id INTEGER PRIMARY KEY, Nombre, Grupo);");
 
-	$cantidad_tipo_incidente = 0;
-	$sql = "SELECT id, Nombre, Grupo FROM Inspeccion_RelevamientoResultado";
-	foreach ($db_remota->query($sql) as $row) {
-		$cantidad_tipo_incidente++;
-		$Id = $row['id'];
-		$Nombre = $row['Nombre'];
-		$Grupo = $row['Grupo'];
+        $cantidad_tipo_incidente = 0;
+        $sql = "SELECT id, Nombre, Grupo FROM Inspeccion_RelevamientoResultado";
+        foreach ($db_remota->query($sql) as $row) {
+            $cantidad_tipo_incidente++;
+            $Id = $row['id'];
+            $Nombre = $row['Nombre'];
+            $Grupo = $row['Grupo'];
 
-		$db_local->exec("INSERT INTO Inspeccion_RelevamientoResultado VALUES ($Id, '$Nombre', '$Grupo');");
-	}
+            $db_local->exec("INSERT INTO Inspeccion_RelevamientoResultado VALUES ($Id, '$Nombre', '$Grupo');");
+        }
 
-	echo "se importaron $cantidad_tipo_incidente registros.</p>";
-	//$db_local->exec("CREATE TABLE Inspeccion_RelevamientoResultadoTipo (Id, Nombre, Grupo);");
-
-
-    // ********************** Envío Relevamientos realizados 2
-	echo "Enviando resultados: ";
-	$sql = "SELECT * FROM Inspeccion_RelevamientoAsignacionResultado";
-	$cantidad_resultado = 0;
-	foreach ($db_local->query($sql) as $row) {
-		$insert = $db_remota->prepare("INSERT INTO Inspeccion_RelevamientoAsignacionResultado
+        echo "se importaron $cantidad_tipo_incidente registros.</p>";
+        //$db_local->exec("CREATE TABLE Inspeccion_RelevamientoResultadoTipo (Id, Nombre, Grupo);");
+        // ********************** Envío Relevamientos realizados 2
+        echo "Enviando resultados: ";
+        $sql = "SELECT * FROM Inspeccion_RelevamientoAsignacionResultado";
+        $cantidad_resultado = 0;
+        foreach ($db_local->query($sql) as $row) {
+            $insert = $db_remota->prepare("INSERT INTO Inspeccion_RelevamientoAsignacionResultado
             (Detalle_id, Resultado_id, Obs, Imagen, Ubicacion, UpdatedAt, CreatedAt, Version)
 			VALUES
             (:detalle_id, :resultado_id, :obs, :imagen,:ubicacion, NOW(), NOW(), 1)");
-		$insert->bindValue('obs', $row['Obs'], PDO::PARAM_STR);
-		$insert->bindValue('imagen', $row['Imagen'], PDO::PARAM_LOB);
-		$insert->bindValue('ubicacion', $row['Ubicacion'], PDO::PARAM_STR);
-        $insert->bindValue('resultado_id', $row['Resultado_id'], PDO::PARAM_INT);
-        $insert->bindValue('detalle_id', $row['Detalle_id'], PDO::PARAM_INT);
-        $insert->execute();
-		if($insert->rowCount()) {
-            $db_remota->exec("UPDATE Inspeccion_RelevamientoAsignacionDetalle SET ResultadosCantidad=ResultadosCantidad+1 WHERE id=" . $row['Detalle_id']);
-            $db_remota->exec("UPDATE Inspeccion_RelevamientoAsignacion SET DetallesResultadosCantidad=DetallesResultadosCantidad+1 WHERE id=" . $row['Asignacion_id']);
-			$db_local->exec("DELETE FROM Inspeccion_RelevamientoAsignacionResultado WHERE id=${row['id']}");
-    		$cantidad_resultado++;
-		}
-	}
-	echo "se exportaron $cantidad_resultado registros.</p>";
+            $insert->bindValue('obs', $row['Obs'], PDO::PARAM_STR);
+            $insert->bindValue('imagen', $row['Imagen'], PDO::PARAM_LOB);
+            $insert->bindValue('ubicacion', $row['Ubicacion'], PDO::PARAM_STR);
+            $insert->bindValue('resultado_id', $row['Resultado_id'], PDO::PARAM_INT);
+            $insert->bindValue('detalle_id', $row['Detalle_id'], PDO::PARAM_INT);
+            $insert->execute();
+            if ($insert->rowCount()) {
+                $db_remota->exec("UPDATE Inspeccion_RelevamientoAsignacionDetalle SET ResultadosCantidad=ResultadosCantidad+1 WHERE id=" . $row['Detalle_id']);
+                $db_remota->exec("UPDATE Inspeccion_RelevamientoAsignacion SET DetallesResultadosCantidad=DetallesResultadosCantidad+1 WHERE id=" . $row['Asignacion_id']);
+                $db_local->exec("DELETE FROM Inspeccion_RelevamientoAsignacionResultado WHERE id=${row['id']}");
+                $cantidad_resultado++;
+            }
+        }
+        echo "se exportaron $cantidad_resultado registros.</p>";
 
 
-    // ********************** Recibir asignaciones nuevas
-	echo "<p>Recibiendo asignaciones: ";
-	$cantidad_incidente = 0;
-	$cantidad_incidente_salteado = 0;
-	$sql = "SELECT * FROM Inspeccion_RelevamientoAsignacionDetalle
+        // ********************** Recibir asignaciones nuevas
+        echo "<p>Recibiendo asignaciones: ";
+        $cantidad_incidente = 0;
+        $cantidad_incidente_salteado = 0;
+        $sql = "SELECT * FROM Inspeccion_RelevamientoAsignacionDetalle
 		WHERE Encargado_id=$IdEncargadoDispositivo
 			AND Relevamiento_id=$IdRelevamientoActual
 			AND ResultadosCantidad=0";
-	foreach ($db_remota->query($sql) as $row) {
-		$Id = $row['id'];
-		$CreatedAt = $row['CreatedAt'];
-		$UpdatedAt = $row['UpdatedAt'];
-		$Version = $row['Version'];
-		$Relevamiento_id = $row['Relevamiento_id'];
-		$Asignacion_id = $row['Asignacion_id'];
-		$Partida_id = $row['Partida_id'];
-		$PartidaSeccion = $row['PartidaSeccion'];
-		$PartidaMacizo = $row['PartidaMacizo'];
-		$PartidaParcela = $row['PartidaParcela'];
-		$PartidaCalleNombre = $row['PartidaCalleNombre'];
-		$PartidaCalleNumero = $row['PartidaCalleNumero'];
-		$Encargado_id = $row['Encargado_id'];
-		$PartidaCalle_id = $row['PartidaCalle_id'] ? $row['PartidaCalle_id'] : 'NULL';
-        $ResultadosCantidad = (int)$row['ResultadosCantidad'];
-		
-		// Si existen resultados para el registro que estoy por importar, no lo importo
-		// para no pisar el trabajo hecho
-		$registro_actual = $db_local->query("SELECT id FROM Inspeccion_RelevamientoAsignacionDetalle WHERE id=$Id AND ResultadosCantidad<>0")->fetchColumn();
-		if($registro_actual == $Id) {
-			$cantidad_incidente_salteado++;
-		} else {
-			$cantidad_incidente++;
-            $sql = "REPLACE INTO Inspeccion_RelevamientoAsignacionDetalle 
+        foreach ($db_remota->query($sql) as $row) {
+            $Id = $row['id'];
+            $CreatedAt = $row['CreatedAt'];
+            $UpdatedAt = $row['UpdatedAt'];
+            $Version = $row['Version'];
+            $Relevamiento_id = $row['Relevamiento_id'];
+            $Asignacion_id = $row['Asignacion_id'];
+            $Partida_id = $row['Partida_id'];
+            $PartidaSeccion = $row['PartidaSeccion'];
+            $PartidaMacizo = $row['PartidaMacizo'];
+            $PartidaParcela = $row['PartidaParcela'];
+            $PartidaCalleNombre = $row['PartidaCalleNombre'];
+            $PartidaCalleNumero = $row['PartidaCalleNumero'];
+            $Encargado_id = $row['Encargado_id'];
+            $PartidaCalle_id = $row['PartidaCalle_id'] ? $row['PartidaCalle_id'] : 'NULL';
+            $ResultadosCantidad = (int) $row['ResultadosCantidad'];
+
+            // Si existen resultados para el registro que estoy por importar, no lo importo
+            // para no pisar el trabajo hecho
+            $registro_actual = $db_local->query("SELECT id FROM Inspeccion_RelevamientoAsignacionDetalle WHERE id=$Id AND ResultadosCantidad<>0")->fetchColumn();
+            if ($registro_actual == $Id) {
+                $cantidad_incidente_salteado++;
+            } else {
+                $cantidad_incidente++;
+                $sql = "REPLACE INTO Inspeccion_RelevamientoAsignacionDetalle
 					(id,
 					CreatedAt,
 					UpdatedAt,
@@ -130,33 +128,32 @@
 					$Encargado_id,
 					$PartidaCalle_id,
                     $ResultadosCantidad)";
-			//echo $sql;
-            $db_local->exec($sql);
-		}
-	}
-	echo "se importaron $cantidad_incidente registros, se saltearon $cantidad_incidente_salteado.</p>";
-    
-    $db_remota->exec("UPDATE Inspeccion_RelevamientoAsignacionResultado 
+                //echo $sql;
+                $db_local->exec($sql);
+            }
+        }
+        echo "se importaron $cantidad_incidente registros, se saltearon $cantidad_incidente_salteado.</p>";
+
+        $db_remota->exec("UPDATE Inspeccion_RelevamientoAsignacionResultado
         SET Inspeccion_RelevamientoAsignacionResultado.Asignacion_id=(
             SELECT Inspeccion_RelevamientoAsignacionDetalle.Asignacion_id FROM Inspeccion_RelevamientoAsignacionDetalle
                 WHERE Inspeccion_RelevamientoAsignacionDetalle.id=Inspeccion_RelevamientoAsignacionResultado.Detalle_id
         );");
-    $db_remota->exec("UPDATE Inspeccion_RelevamientoAsignacion
+        $db_remota->exec("UPDATE Inspeccion_RelevamientoAsignacion
         SET DetallesResultadosCantidad=(
             SELECT COUNT(id) FROM Inspeccion_RelevamientoAsignacionResultado
                 WHERE Inspeccion_RelevamientoAsignacionResultado.Asignacion_id=Inspeccion_RelevamientoAsignacion.id
         );");
-    include_once 'actualizar_db.php'
+        include_once 'actualizar_db.php'
+        ?>
 
-?>
-
-<script>
-window.setTimeout(RedireccionarSinc, 10000);
-function RedireccionarSinc() {
-    window.location='listado.php';
-}
-</script>
-</div>
+        <script>
+            window.setTimeout(RedireccionarSinc, 10000);
+            function RedireccionarSinc() {
+                window.location = 'listado.php';
+            }
+        </script>
+    </div>
 
 </body>
 
