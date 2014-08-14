d52d50228d0d8972d9772942fc1dd883579bd2fe
Ezequiel Riquelme <rezequiel.tdf@gmail.com>
2014-08-14 12:59:42 +0000

    Former-commit-id: 47759114562866f003c4db5449ab8145b4bb127d

---
+++ src/Yacare/ObrasParticularesBundle/Controller/MatriculaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Yacare\ObrasParticularesBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Controlador de agentes.
+ * 
+ * @author Ernesto Carrea <ernestocarrea@gmail.com>
+ * 
+ * @Route("matricula/")
+ */
+class MatriculaController extends \Tapir\BaseBundle\Controller\AbmController
+{
+    function IniciarVariables() {
+        parent::IniciarVariables();
+
+        $this->BuscarPor = 'id, p.NombreVisible, p.DocumentoNumero';
+        $this->Joins[] = ' JOIN r.Persona p';
+    }
+}
