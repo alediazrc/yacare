ede2173129b422317dc212fc2f4888c3d724a331
Ezequiel Riquelme <rezequiel.tdf@gmail.com>
2014-08-21 14:50:24 +0000

    Former-commit-id: 5dc894d2466e66d4c0e1ae214733c2d032b654db

---
+++ src/Yacare/ObrasParticularesBundle/Form/MatricualType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Yacare\ObrasParticularesBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+class Matricula extends \Yacare\TramitesBundle\Form\TramiteType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder
+            ->add('Nombre', 'entity_id', array(
+                'label' => 'Nombre',
+                'class' => 'Yacare\BaseBundle\Entity\Persona',
+                'required'  => true
+                ))
+            ->add('Partida', 'entity_id', array(
+                'label' => 'Partida',
+                'class' => 'Yacare\CatastroBundle\Entity\Partida',
+                'required' => true))
+            ->add('Profesion', 'choice', array(
+                'label' => 'Profesion',
+                'required'  => true,
+                'choices' => array(
+                    'Ingeniero Civil' => 'Ingeniero Civil',
+                    'Arquitecto' => 'Arquitecto',
+                    'Maestro Mayor de Obras' => 'M.M Obras',
+                    'Tecnico Constructor' => 'T. Constructor',
+                    'Otro' => 'Otro'
+                    )
+                ))
+            ->add('Estado', 'choice', array(
+                'label' => 'Estado',
+                'required'  => true,
+                'choices' => array(
+                    'Habilitado' => 'Habilitado',
+                    'No Habilitado' => 'No Habilitado',
+                    
+                    )
+                ))
+                ->add('FechaVencimiento', 'date', array(
+                'years' => range(1900, 2099),
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'label' => 'Fecha de Vencimiento'))
+                ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Yacare\ObrasParticularesBundle\Entity\Matricula',
+            'cascade_validation' => true,
+        ));
+    }
+
+    public function getName()
+    {
+        return 'yacare_obrasparticularesbundle_matriculatype';
+    }
+}
