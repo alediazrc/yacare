e6fb675ae058b9c9c70f70689973288e2893f83e
Ernesto Carrea <equistango@gmail.com>
2014-09-18 18:06:13 +0000

    Former-commit-id: 8b846ccf99f6de3f4d60913cde8582e5b468500a

--- src/Yacare/RecursosHumanosBundle/Entity/Agente.php
+++ src/Yacare/RecursosHumanosBundle/Entity/Agente.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Yacare\RecursosHumanosBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -9,199 +10,237 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @author Ernesto Carrea <ernestocarrea@gmail.com>
  *        
- * @ORM\Table(name="Rrhh_Agente", uniqueConstraints={@ORM\UniqueConstraint(name="ImportSrcId", columns={"ImportSrc", "ImportId"})})
- * @ORM\Entity(repositoryClass="Tapir\BaseBundle\Entity\TapirBaseRepository")
+ *         @ORM\Table(name="Rrhh_Agente", uniqueConstraints={@ORM\UniqueConstraint(name="ImportSrcId", columns={"ImportSrc", "ImportId"})})
+ *         @ORM\Entity(repositoryClass="Tapir\BaseBundle\Entity\TapirBaseRepository")
  */
-class Agente
-{
-    use\Tapir\BaseBundle\Entity\ConId;
-    use\Tapir\BaseBundle\Entity\Versionable;
-    use\Tapir\BaseBundle\Entity\Suprimible;
-    use\Tapir\BaseBundle\Entity\Importable;
-    use\Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
-    
-    /*
-     * CREATE VIEW rr_hh_agentes AS SELECT * FROM rr_hh.agentes; CREATE OR REPLACE VIEW yacare.Rrhh_Agente AS SELECT agentes.legajo AS id, agentes.fechaingre AS FechaIngreso, agentes.nombre AS NombreVisible, agentes.username, agentes.salt, agentes.password, agentes.is_active, agentes.NombreSolo as Nombre, agentes.Apellido, agentes.email FROM rr_hh.agentes; ALTER TABLE rr_hh.agentes ADD username VARCHAR(25) NOT NULL DEFAULT '', ADD salt VARCHAR(32) NOT NULL DEFAULT '', ADD password VARCHAR(40) NOT NULL DEFAULT '', ADD NombreSolo VARCHAR(255) NOT NULL DEFAULT '', ADD Apellido VARCHAR(255) NOT NULL DEFAULT '', CHANGE fechaingre fechaingre DATE NOT NULL, CHANGE nombre nombre VARCHAR(255) NOT NULL DEFAULT '', CHANGE email email VARCHAR(255) NOT NULL DEFAULT ''; UPDATE yacare.Rrhh_Agente SET salt=MD5(RAND()) WHERE salt=''; UPDATE rr_hh.agentes SET Apellido=TRIM(SUBSTRING_INDEX(nombre, ' ', 1)) WHERE NombreSolo=''; UPDATE rr_hh.agentes SET NombreSolo=TRIM(TRIM(LEADING Apellido FROM nombre)) WHERE NombreSolo='';
-     */
-    
-    /**
-     * La persona asociada.
-     *
-     * @ORM\ManyToOne(targetEntity="Yacare\BaseBundle\Entity\Persona")
-     * @ORM\JoinColumn(referencedColumnName="id", nullable=false)
-     */
-    protected $Persona;
-
-    /**
-     * La categoría actual.
-     *
-     * @var $Categoria
-     * 
-     * @ORM\Column(type="integer", nullable=false)
-     */
-    private $Categoria;
-
-    /**
-     * TODO: Verificar el significado de este campo en "Gestion.exe" / con Daniel Camargo.
-     *
-     * @var $Situacion
-     * 
-     * @ORM\Column(type="integer", nullable=false)
-     */
-    private $Situacion;
-
-    /**
-     * La función que cumple, en formato de texto libre. 
-     *
-     * @var $Funcion
-     * 
-     * @ORM\Column(type="string")
-     */
-    private $Funcion;
-
-    /**
-     * La fecha de ingreso.
-     *
-     * @ORM\Column(type="date", nullable=true)
-     * @Assert\Type("\DateTime")
-     */
-    private $FechaIngreso;
-
-    /**
-     * La fecha de baja, o NULL si todavía está activo.
-     *
-     * @ORM\Column(type="date", nullable=true)
-     * @Assert\Type("\DateTime")
-     */
-    private $FechaBaja;
-
-    /**
-     * El departamento en el cual se desempeña.
-     *
-     * @var $Departamento
-     * 
-     * @ORM\ManyToOne(targetEntity="\Yacare\OrganizacionBundle\Entity\Departamento")
-     * @ORM\JoinColumn(referencedColumnName="id", nullable=true)
-     */
-    protected $Departamento;
-    
-
-    public function __toString()
-    {
-        return $this->getPersona()->getNombreVisible();
-    }
-    
-    
-
-    /**
-     * @ignore
-     */
-    public function getPersona()
-    {
-        return $this->Persona;
-    }
-
-    /**
-     * @ignore
-     */
-    public function setPersona($Persona)
-    {
-        $this->Persona = $Persona;
-    }
-
-    /**
-     * @ignore
-     */
-    public function getCategoria()
-    {
-        return $this->Categoria;
-    }
-
-    /**
-     * @ignore
-     */
-    public function setCategoria($Categoria)
-    {
-        $this->Categoria = $Categoria;
-    }
-
-    /**
-     * @ignore
-     */
-    public function getSituacion()
-    {
-        return $this->Situacion;
-    }
-
-    /**
-     * @ignore
-     */
-    public function setSituacion($Situacion)
-    {
-        $this->Situacion = $Situacion;
-    }
-
-    /**
-     * @ignore
-     */
-    public function getFuncion()
-    {
-        return $this->Funcion;
-    }
-
-    /**
-     * @ignore
-     */
-    public function setFuncion($Funcion)
-    {
-        $this->Funcion = $Funcion;
-    }
-
-    /**
-     * @ignore
-     */
-    public function getFechaIngreso()
-    {
-        return $this->FechaIngreso;
-    }
-
-    /**
-     * @ignore
-     */
-    public function setFechaIngreso(\DateTime $FechaIngreso = null)
-    {
-        $this->FechaIngreso = $FechaIngreso;
-    }
-
-    /**
-     * @ignore
-     */
-    public function getFechaBaja()
-    {
-        return $this->FechaBaja;
-    }
-
-    /**
-     * @ignore
-     */
-    public function setFechaBaja(\DateTime $FechaBaja = null)
-    {
-        $this->FechaBaja = $FechaBaja;
-    }
-
-    /**
-     * @ignore
-     */
-    public function getDepartamento()
-    {
-        return $this->Departamento;
-    }
-
-    /**
-     * @ignore
-     */
-    public function setDepartamento($Departamento)
-    {
-        $this->Departamento = $Departamento;
-    }
+class Agente {
+	use \Tapir\BaseBundle\Entity\ConId;
+	use \Tapir\BaseBundle\Entity\Versionable;
+	use \Tapir\BaseBundle\Entity\Suprimible;
+	use \Tapir\BaseBundle\Entity\Importable;
+	use \Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
+	
+	/*
+	 * CREATE VIEW rr_hh_agentes AS SELECT * FROM rr_hh.agentes; CREATE OR REPLACE VIEW yacare.Rrhh_Agente AS SELECT agentes.legajo AS id, agentes.fechaingre AS FechaIngreso, agentes.nombre AS NombreVisible, agentes.username, agentes.salt, agentes.password, agentes.is_active, agentes.NombreSolo as Nombre, agentes.Apellido, agentes.email FROM rr_hh.agentes; ALTER TABLE rr_hh.agentes ADD username VARCHAR(25) NOT NULL DEFAULT '', ADD salt VARCHAR(32) NOT NULL DEFAULT '', ADD password VARCHAR(40) NOT NULL DEFAULT '', ADD NombreSolo VARCHAR(255) NOT NULL DEFAULT '', ADD Apellido VARCHAR(255) NOT NULL DEFAULT '', CHANGE fechaingre fechaingre DATE NOT NULL, CHANGE nombre nombre VARCHAR(255) NOT NULL DEFAULT '', CHANGE email email VARCHAR(255) NOT NULL DEFAULT ''; UPDATE yacare.Rrhh_Agente SET salt=MD5(RAND()) WHERE salt=''; UPDATE rr_hh.agentes SET Apellido=TRIM(SUBSTRING_INDEX(nombre, ' ', 1)) WHERE NombreSolo=''; UPDATE rr_hh.agentes SET NombreSolo=TRIM(TRIM(LEADING Apellido FROM nombre)) WHERE NombreSolo='';
+	 */
+	
+	/**
+	 * La persona asociada.
+	 *
+	 * @ORM\ManyToOne(targetEntity="Yacare\BaseBundle\Entity\Persona")
+	 * @ORM\JoinColumn(referencedColumnName="id", nullable=false)
+	 */
+	protected $Persona;
+	
+	/**
+	 * La categoría actual.
+	 *
+	 * @var $Categoria @ORM\Column(type="integer", nullable=false)
+	 */
+	private $Categoria;
+	
+	/**
+	 * TODO: Verificar el significado de este campo en "Gestion.exe" / con Daniel Camargo.
+	 *
+	 * @var $Situacion @ORM\Column(type="integer", nullable=false)
+	 */
+	private $Situacion;
+	
+	/**
+	 * La función que cumple, en formato de texto libre.
+	 *
+	 *
+	 * @var $Funcion @ORM\Column(type="string")
+	 */
+	private $Funcion;
+	
+	/**
+	 * La fecha de ingreso.
+	 *
+	 * @ORM\Column(type="date", nullable=true)
+	 * @Assert\Type("\DateTime")
+	 */
+	private $FechaIngreso;
+	
+	/**
+	 * La fecha de baja, o NULL si todavía está activo.
+	 *
+	 * @ORM\Column(type="date", nullable=true)
+	 * @Assert\Type("\DateTime")
+	 */
+	private $FechaBaja;
+	
+	/**
+	 * El departamento en el cual se desempeña.
+	 *
+	 * @var $Departamento @ORM\ManyToOne(targetEntity="\Yacare\OrganizacionBundle\Entity\Departamento")
+	 *      @ORM\JoinColumn(referencedColumnName="id", nullable=true)
+	 */
+	protected $Departamento;
+	public function __toString() {
+		return $this->getPersona ()->getNombreVisible ();
+	}
+	public static function SituacionesNombres($rango) {
+		switch ($rango) {
+			case 0 :
+				return 'Normal';
+			case 1 :
+				return 'Licencia sin goce de haberes';
+			case 2	 :
+				return 'Baja';
+			case 3 :
+				return 'Adcripción a otros organismos';
+			case 4 :
+				return 'Largo tratamiento al 50%';
+			case 5 :
+				return 'Ad-honorem';
+			case 6 :
+				return 'Adscripción de otros organismos';
+			case 7 :
+				return 'Pasantía rentada';
+			case 8 :
+				return 'Pasantía no rentada';
+			case 9 :
+				return 'Largo tratamiento al 100%';
+			case 10 :
+				return 'Largo tratamiento sin goce de haberes';
+			case 11 :
+				return 'Haberes 95% pasividad';
+			case 12 :
+				return 'Gabinete';
+			case 13 :
+				return 'Suspensión';
+			default :
+				return '';
+		}
+	}
+	public function getSituacionNombre() {
+		return Agente::SituacionesNombres ( $this->getSituacion () );
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function getPersona() {
+		return $this->Persona;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function setPersona($Persona) {
+		$this->Persona = $Persona;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function getCategoria() {
+		return $this->Categoria;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function setCategoria($Categoria) {
+		$this->Categoria = $Categoria;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function getSituacion() {
+		return $this->Situacion;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function setSituacion($Situacion) {
+		$this->Situacion = $Situacion;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function getFuncion() {
+		return $this->Funcion;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function setFuncion($Funcion) {
+		$this->Funcion = $Funcion;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function getFechaIngreso() {
+		return $this->FechaIngreso;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function setFechaIngreso(\DateTime $FechaIngreso = null) {
+		$this->FechaIngreso = $FechaIngreso;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function getFechaBaja() {
+		return $this->FechaBaja;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function setFechaBaja(\DateTime $FechaBaja = null) {
+		$this->FechaBaja = $FechaBaja;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function getDepartamento() {
+		return $this->Departamento;
+	}
+	
+	/**
+	 *
+	 * @ignore
+	 *
+	 */
+	public function setDepartamento($Departamento) {
+		$this->Departamento = $Departamento;
+	}
 }
