a3029f471898bc1bf244cb2c7d15b9d8db03d875
Ernesto Carrea <equistango@gmail.com>
2014-09-24 14:27:01 +0000

    Former-commit-id: 0228c081aec11e656616ace81d969ae1472c8c94

--- src/Tapir/BaseBundle/Controller/BaseController.php
+++ src/Tapir/BaseBundle/Controller/BaseController.php
@@ -160,9 +160,11 @@ abstract class BaseController extends Controller
             }
         }
         
-        /*
-         * $val = $request->query->get('page'); if ($val) { $valorInicial['arrastre']['page'] = $val; }
-         */
+        // Arrastro el valor de la variable page
+        $val = $request->query->get('page');
+        if ($val) {
+            $valorInicial['arrastre']['page'] = $val;
+        }
         
         if (! isset($valorInicial['arrastre'])) {
             $valorInicial['arrastre']['d'] = '';
