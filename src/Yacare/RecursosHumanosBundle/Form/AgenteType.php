4d1d8c100fe684575a95d18cb86985b0603cd462
Ernesto Carrea <equistango@gmail.com>
2014-10-08 15:43:36 +0000

    Former-commit-id: 1ce243f105008c325ccc13b39d4b45a60dd5b98e

--- src/Yacare/RecursosHumanosBundle/Form/AgenteType.php
+++ src/Yacare/RecursosHumanosBundle/Form/AgenteType.php
@@ -11,27 +11,26 @@ class AgenteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('id', null, array(
-            'label' => 'Legajo',
-            'read_only' => true
-        ))
-            ->add('Nombre', null, array(
-            'label' => 'Nombres',
-            'required' => true
-        ))
-            ->add('Apellido', null, array(
-            'label' => 'Apellidos',
-            'required' => true
-        ))
-            ->add('username', null, array(
-            'label' => 'Nombre de usuario'
-        ))
+                'label' => 'Legajo',
+                'read_only' => true
+            ))
+            ->add('Grupos', 'entity', array(
+                'label' => 'Grupos',
+                'class' => 'YacareRecursosHumanosBundle:AgenteGrupo',
+                'multiple' => true,
+                'required' => false
+            ))
             ->add('FechaIngreso', 'date', array(
-            'years' => range(1900, 2099),
-            'input' => 'datetime',
-            'widget' => 'single_text',
-            'format' => 'dd/MM/yyyy',
-            'label' => 'Fecha de ingreso'
-        ));
+                'years' => range(1900, 2099),
+                'input' => 'datetime',
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'label' => 'Fecha de ingreso'
+            ))
+            ->add('Persona', new \Yacare\BaseBundle\Form\PersonaType(), array(
+                'label' => 'Persona asociada',
+            ))
+            ;
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
