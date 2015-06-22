4e7b15772c653067c75a33a4a7f8c84d4f409587
Alejandro Diaz <alediaz.rc@gmail.com>
2015-06-22 13:40:52 +0000

    Former-commit-id: ce2f68116030363f9e69922c782f2213d1830270

--- src/Yacare/BaseBundle/Controller/DispositivoRastreadorGpsController.php
+++ src/Yacare/BaseBundle/Controller/DispositivoRastreadorGpsController.php
@@ -37,8 +37,9 @@ class DispositivoRastreadorGpsController extends DispositivoController
         }
         
         $em = $this->getEm();
-        $UltimoRastreo = $em->getRepository('Yacare\BaseBundle\Entity\DispositivoRastreo')->findBy(
-            array('Dispositivo' => $id), array('id' => 'DESC'), 1);
+        $UltimoRastreo = $em->getRepository('Yacare\BaseBundle\Entity\DispositivoRastreo')->findBy(array(
+            'Dispositivo' => $id), array(
+            'id' => 'DESC'), 1);
         
         if (count($UltimoRastreo) == 1) {
             // Si es un array de un 1 elemento, lo convierto en un elemento plano.
@@ -54,8 +55,9 @@ class DispositivoRastreadorGpsController extends DispositivoController
             $map->setCenter(- 53.789858, - 67.692911, true);
         }
         
-        $UltimosRastreos = $em->getRepository('Yacare\BaseBundle\Entity\DispositivoRastreo')->findBy(
-            array('Dispositivo' => $id), array('id' => 'DESC'), 100);
+        $UltimosRastreos = $em->getRepository('Yacare\BaseBundle\Entity\DispositivoRastreo')->findBy(array(
+            'Dispositivo' => $id), array(
+            'id' => 'DESC'), 100);
         
         if ($UltimosRastreos) {
             $polyline = new \Ivory\GoogleMap\Overlays\Polyline();
@@ -72,13 +74,11 @@ class DispositivoRastreadorGpsController extends DispositivoController
             $map->addPolyline($polyline);
         }
         
-        
         $mapHelper = new MapHelper();
         
         $mapHelper->setExtensionHelper('gps_extension_helper', new GpsExtensionHelper());
         
         $output = $mapHelper->renderJavascripts($map);
-        
         
         $res['map'] = $map;
         $res['js'] = $output;
@@ -93,54 +93,51 @@ class DispositivoRastreadorGpsController extends DispositivoController
      */
     public function coordjsonAction(Request $request)
     {
-    	
-    	
-        //obtenemos los ids de los marcadores enviados por POST
-    	$rastreadores = $request->request->get('id_ras');
-        	
-    	
-    	
-    	$x = array();
-    	
-    	$y = array();
-    	
-    	$em = $this->getEm();
-    	
-    	
-    	//iteramos por cada marcador en el mapa y buscamos las nuevas coordenadas
-    	foreach ($rastreadores as $rastreador){
-    	
-    		$UltimoRastreo = $em->getRepository('Yacare\BaseBundle\Entity\DispositivoRastreo')->findBy(
-    				array('Dispositivo' => $rastreador), array('id' => 'DESC'), 1);
-    		
-    		if (count($UltimoRastreo) == 1) {
-    			// Si es un array de un 1 elemento, lo convierto en un elemento plano.
-    			$UltimoRastreo = $UltimoRastreo[0];
-    		}
-
-
-		//TODO: quitar el rand para produccion    		
-    		$sumX = $UltimoRastreo->getUbicacion()->getX() + (rand(0,10)/100);
-    		
-    		$sumY = $UltimoRastreo->getUbicacion()->getY() + (rand(0,10)/100);
-    		
-    		//asignamos las coordenadas en dos array X e Y
-    		array_push($x, $sumX);
-    		
-    		array_push($y, $sumY);
-    	}
-    	
         
-        $res = array('x' => $x,'y' => $y);
+        // obtenemos los ids de los marcadores enviados por POST
+        $rastreadores = $request->request->get('id_ras');
+        
+        $x = array();
+        
+        $y = array();
+        
+        $em = $this->getEm();
+        
+        // iteramos por cada marcador en el mapa y buscamos las nuevas coordenadas
+        foreach ($rastreadores as $rastreador) {
+            
+            $UltimoRastreo = $em->getRepository('Yacare\BaseBundle\Entity\DispositivoRastreo')->findBy(array(
+                'Dispositivo' => $rastreador), array(
+                'id' => 'DESC'), 1);
+            
+            if (count($UltimoRastreo) == 1) {
+                // Si es un array de un 1 elemento, lo convierto en un elemento plano.
+                $UltimoRastreo = $UltimoRastreo[0];
+            }
+            
+            // TODO: quitar el rand para produccion
+            $sumX = $UltimoRastreo->getUbicacion()->getX();
+            
+            $sumY = $UltimoRastreo->getUbicacion()->getY();
+            
+            // asignamos las coordenadas en dos array X e Y
+            array_push($x, $sumX);
+            
+            array_push($y, $sumY);
+        }
+        
+        $res = array(
+            'x' => $x,
+            'y' => $y);
         
         /*
          * En este punto sabemos que al marcador[0] le corresponden las coordenadas x[0] e y[0]
          * y asi con todos
-        */
-    	
+         */
+        
         return new JsonResponse($res);
     }
-    
+
     /**
      * @Route ("vertodos/")
      * @Template("YacareBaseBundle:DispositivoRastreadorGps:ver.html.twig")
@@ -161,8 +158,9 @@ class DispositivoRastreadorGpsController extends DispositivoController
                 $entity->setObs('Serie ' . $entity->getNumeroSerie());
             }
             
-            $UltimoRastreo = $em->getRepository('Yacare\BaseBundle\Entity\DispositivoRastreo')->findBy(
-                array('Dispositivo' => $id), array('id' => 'DESC'), 1);
+            $UltimoRastreo = $em->getRepository('Yacare\BaseBundle\Entity\DispositivoRastreo')->findBy(array(
+                'Dispositivo' => $id), array(
+                'id' => 'DESC'), 1);
             
             if ($UltimoRastreo) {
                 $UltimoRastreo = $UltimoRastreo[0];
@@ -193,15 +191,18 @@ class DispositivoRastreadorGpsController extends DispositivoController
     {
         $map = $this->get('ivory_google_map.map');
         
-        
         $map->setMapOption('zoom', 30);
         $map->setAsync(true);
         $map->setAutoZoom(true);
         
-        $map->setMapOptions(
-            array('disableDefaultUI' => true,'disableDoubleClickZoom' => true,'mapTypeId' => 'roadmap'));
+        $map->setMapOptions(array(
+            'disableDefaultUI' => true,
+            'disableDoubleClickZoom' => true,
+            'mapTypeId' => 'roadmap'));
         
-        $map->setStylesheetOptions(array('width' => '100%','height' => '480px'));
+        $map->setStylesheetOptions(array(
+            'width' => '100%',
+            'height' => '480px'));
         
         $map->setLanguage('es');
         
@@ -227,8 +228,10 @@ class DispositivoRastreadorGpsController extends DispositivoController
         // $infoWindow->setAutoOpen(true);
         // $infoWindow->setOpenEvent(\Ivory\GoogleMap\Events\MouseEvent::CLICK);
         $infoWindow->setAutoClose(false);
-        $infoWindow->setOptions(
-            array('disableAutoPan' => false,'zIndex' => 10,'maxWidth' => 100));
+        $infoWindow->setOptions(array(
+            'disableAutoPan' => false,
+            'zIndex' => 10,
+            'maxWidth' => 100));
         
         // Configuración de las opciones del marcador a incorporar
         $marker = new \Ivory\GoogleMap\Overlays\Marker();
@@ -237,8 +240,10 @@ class DispositivoRastreadorGpsController extends DispositivoController
             ->getX(), $UltimoRastreo->getUbicacion()
             ->getY(), true);
         $marker->setAnimation(\Ivory\GoogleMap\Overlays\Animation::DROP);
-        $marker->setOptions(
-            array('clickable' => true,'flat' => true,'title' => (string) $entity));
+        $marker->setOptions(array(
+            'clickable' => true,
+            'flat' => true,
+            'title' => (string) $entity));
         
         // Incorporo la ventana de información como una propiedad más al marcador.
         $marker->setInfoWindow($infoWindow);
--- src/Yacare/BaseBundle/Resources/Extensions/GpsExtensionHelper.php
+++ src/Yacare/BaseBundle/Resources/Extensions/GpsExtensionHelper.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Yacare\BaseBundle\Resources\Extensions;
 
 use Ivory\GoogleMap\Helper\Extension\ExtensionHelperInterface;
@@ -7,65 +6,64 @@ use Ivory\GoogleMap\Map;
 
 class GpsExtensionHelper implements ExtensionHelperInterface
 {
-	/**
-	 * @inheritdoc
-	 */
-	public function renderLibraries(Map $map)
-	{
-		return null;
-	}
-	
-	/**
-	 * @inheritdoc
-	 */
-	public function renderBefore(Map $map)
-	{
-		return null;
-	}
-	
-	/**
-	 * @inheritdoc
-	 */
-	public function renderAfter(Map $map)
-	{
-		//Obtengo todos los marcadores del mapa
-		$markers = $map->getMarkers();
-		
-		$mapVar = $map->getJavaScriptVariable();
-		
-		$polylines = $map->getPolylines();
-		
-		$polyVars = array();
-		
-		//Obtengo las variables (String) de cada uno de los marcadores y se almacenan en un array. 
-		$variables = array();
-		
-		foreach ($markers as $mark){
-			$variable = $mark->getJavaScriptVariable();
-			array_push($variables, $variable);
-		}
-		
-		//Obtengo las variables de las polylines.
-		foreach ($polylines as $polyline){
-			array_push($polyVars, $polyline->getJavaScriptVariable());
-		}
-		
-		//Guardo las variables en String $ret, para ser el retorno del metodo
-		$ret = 'markers = new Array(); map = ' . $mapVar . ';' . ' polylines = new Array();';
-		$i = 0;
-		
-		foreach($variables as $variable){
-			$ret = $ret . 'markers['. $i .'] = ' . $variable . ';' . PHP_EOL;
-			$i++;
-		}
-		
-		$i = 0;
-		foreach($polyVars as $polyline){
-			$ret = $ret . 'polylines['. $i .'] = ' . $polyline . ';' . PHP_EOL;			
-		}
-		
-		return $ret;
-	}
-	
 
+    /**
+     * @inheritdoc
+     */
+    public function renderLibraries(Map $map)
+    {
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function renderBefore(Map $map)
+    {
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function renderAfter(Map $map)
+    {
+        // Obtengo todos los marcadores del mapa
+        $markers = $map->getMarkers();
+        
+        $mapVar = $map->getJavaScriptVariable();
+        
+        $polylines = $map->getPolylines();
+        
+        $polyVars = array();
+        
+        // Obtengo las variables (String) de cada uno de los marcadores y se almacenan en un array.
+        $variables = array();
+        
+        foreach ($markers as $mark) {
+            $variable = $mark->getJavaScriptVariable();
+            array_push($variables, $variable);
+        }
+        
+        // Obtengo las variables de las polylines.
+        foreach ($polylines as $polyline) {
+            array_push($polyVars, $polyline->getJavaScriptVariable());
+        }
+        
+        // Guardo las variables en String $ret, para ser el retorno del metodo
+        $ret = 'markers = new Array(); map = ' . $mapVar . ';' . ' polylines = new Array();';
+        $i = 0;
+        
+        foreach ($variables as $variable) {
+            $ret = $ret . 'markers[' . $i . '] = ' . $variable . ';' . PHP_EOL;
+            $i ++;
+        }
+        
+        $i = 0;
+        foreach ($polyVars as $polyline) {
+            $ret = $ret . 'polylines[' . $i . '] = ' . $polyline . ';' . PHP_EOL;
+        }
+        
+        return $ret;
+    }
 }
