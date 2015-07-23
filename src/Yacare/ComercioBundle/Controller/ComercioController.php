84e4c2c8f622b56d1c8e0b908100e6f1ba285044
Alejandro Diaz <alediaz.rc@gmail.com>
2015-07-23 14:56:23 +0000

    Former-commit-id: 3eb300683f9ad1b840eca1019aaf682ffb6406ce

--- src/Yacare/ComercioBundle/Controller/ComercioController.php
+++ src/Yacare/ComercioBundle/Controller/ComercioController.php
@@ -3,6 +3,7 @@ namespace Yacare\ComercioBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("comercio/")
