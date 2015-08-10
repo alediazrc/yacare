0dc1301dd630e4d98a9fe24b3255fd115a4bd8af
Alejandro Diaz <alediaz.rc@gmail.com>
2015-08-10 14:09:07 +0000

    Former-commit-id: e601ec6207da46de2feed2d7b7c1bf91e7b8fb30

--- src/Yacare/ComercioBundle/Controller/ComercioController.php
+++ src/Yacare/ComercioBundle/Controller/ComercioController.php
@@ -3,7 +3,9 @@ namespace Yacare\ComercioBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
+use Yacare\ComercioBundle\Entity\Actividad;
 
 /**
  * @Route("comercio/")
@@ -19,5 +21,45 @@ class ComercioController extends \Tapir\BaseBundle\Controller\AbmController
     function altamanualAction(Request $request)
     {
         return $this->ArrastrarVariables($request);
+    }
+
+    /**
+     * Interviene las actividades antes de persistirlas en un comercio.
+     * 
+     * @see \Tapir\BaseBundle\Controller\AbmController::guardarActionPrePersist()
+     */
+    public function guardarActionPrePersist($entity, $flag)
+    {
+        $flag = false;
+        
+        if ($entity->getActividad6() && ! $entity->getActividad5()) {
+            $entity->setActividad5($entity->getActividad6());
+            $entity->setActividad6(null);
+            $flag = true;
+        }
+        
+        if ($entity->getActividad5() && ! $entity->getActividad4()) {
+            $entity->setActividad4($entity->getActividad5());
+            $entity->setActividad5(null);
+            $flag = true;
+        }
+        
+        if ($entity->getActividad4() && ! $entity->getActividad3()) {
+            $entity->setActividad3($entity->getActividad4());
+            $entity->setActividad4(null);
+            $flag = true;
+        }
+        
+        if ($entity->getActividad3() && ! $entity->getActividad2()) {
+            $entity->setActividad2($entity->getActividad3());
+            $entity->setActividad3(null);
+            $flag = true;
+        }
+        
+        if ($flag) {
+            return $this->guardarActionPrePersist($entity, $flag);
+        } else {
+            return array();
+        }
     }
 }
--- src/Yacare/ComercioBundle/Entity/ConActividades.php
+++ src/Yacare/ComercioBundle/Entity/ConActividades.php
@@ -24,20 +24,19 @@ trait ConActividades
      * @ORM\JoinColumn(referencedColumnName="id")
      */
     protected $Actividad3;
-    
-    
+
     /**
      * @ORM\ManyToOne(targetEntity="Yacare\ComercioBundle\Entity\Actividad")
      * @ORM\JoinColumn(referencedColumnName="id")
      */
     protected $Actividad4;
-    
+
     /**
      * @ORM\ManyToOne(targetEntity="Yacare\ComercioBundle\Entity\Actividad")
      * @ORM\JoinColumn(referencedColumnName="id")
      */
     protected $Actividad5;
-    
+
     /**
      * @ORM\ManyToOne(targetEntity="Yacare\ComercioBundle\Entity\Actividad")
      * @ORM\JoinColumn(referencedColumnName="id")
@@ -54,8 +53,8 @@ trait ConActividades
         $Activ6 = $this->getActividad6();
         
         return ($Activ1 != null && $Activ1->getRequiereDeyma()) || ($Activ2 != null && $Activ2->getRequiereDeyma()) ||
-             ($Activ3 != null && $Activ3->getRequiereDeyma())|| ($Activ4 != null && $Activ4->getRequiereDeyma()
-             || ($Activ5 != null && $Activ5->getRequiereDeyma())|| ($Activ6 != null && $Activ6->getRequiereDeyma()));
+                 ($Activ3 != null && $Activ3->getRequiereDeyma()) || ($Activ4 != null && $Activ4->getRequiereDeyma()) ||
+                 ($Activ5 != null && $Activ5->getRequiereDeyma()) || ($Activ6 != null && $Activ6->getRequiereDeyma());
     }
 
     public function getRequiereDbeh()
@@ -68,8 +67,8 @@ trait ConActividades
         $Activ6 = $this->getActividad6();
         
         return ($Activ1 != null && $Activ1->getRequiereDbeh()) || ($Activ2 != null && $Activ2->getRequiereDbeh()) ||
-             ($Activ3 != null && $Activ3->getRequiereDbeh())|| ($Activ4 != null && $Activ4->getRequiereDbeh()
-             || ($Activ5 != null && $Activ5->getRequiereDbeh())|| ($Activ6 != null && $Activ6->getRequiereDbeh()));
+                 ($Activ3 != null && $Activ3->getRequiereDbeh()) || ($Activ4 != null && $Activ4->getRequiereDbeh()) ||
+                 ($Activ5 != null && $Activ5->getRequiereDbeh()) || ($Activ6 != null && $Activ6->getRequiereDbeh());
     }
 
     public function getRequiereInfEscolar()
@@ -81,13 +80,16 @@ trait ConActividades
         $Activ5 = $this->getActividad5();
         $Activ6 = $this->getActividad6();
         
-        return ($Activ1 != null && $Activ1->getRequiereInfEscolar()) || ($Activ2 != null && $Activ2->getRequiereInfEscolar()) ||
-             ($Activ3 != null && $Activ3->getRequiereInfEscolar())|| ($Activ4 != null && $Activ4->getRequiereInfEscolar()
-             || ($Activ5 != null && $Activ5->getRequiereInfEscolar())|| ($Activ6 != null && $Activ6->getRequiereInfEscolar()));
+        return ($Activ1 != null && $Activ1->getRequiereInfEscolar()) ||
+                 ($Activ2 != null && $Activ2->getRequiereInfEscolar()) ||
+                 ($Activ3 != null && $Activ3->getRequiereInfEscolar()) ||
+                 ($Activ4 != null && $Activ4->getRequiereInfEscolar()) ||
+                 ($Activ5 != null && $Activ5->getRequiereInfEscolar()) ||
+                 ($Activ6 != null && $Activ6->getRequiereInfEscolar());
     }
 
     public function getRequiereCamaraGrasa()
-    { 
+    {
         $Activ1 = $this->getActividad1();
         $Activ2 = $this->getActividad2();
         $Activ3 = $this->getActividad3();
@@ -95,9 +97,12 @@ trait ConActividades
         $Activ5 = $this->getActividad5();
         $Activ6 = $this->getActividad6();
         
-        return ($Activ1 != null && $Activ1->getRequiereInfEscolar()) || ($Activ2 != null && $Activ2->getRequiereInfEscolar()) ||
-             ($Activ3 != null && $Activ3->getRequiereInfEscolar())|| ($Activ4 != null && $Activ4->getRequiereInfEscolar()
-             || ($Activ5 != null && $Activ5->getRequiereInfEscolar())|| ($Activ6 != null && $Activ6->getRequiereInfEscolar()));
+        return ($Activ1 != null && $Activ1->getRequiereInfEscolar()) ||
+                 ($Activ2 != null && $Activ2->getRequiereInfEscolar()) ||
+                 ($Activ3 != null && $Activ3->getRequiereInfEscolar()) ||
+                 ($Activ4 != null && $Activ4->getRequiereInfEscolar()) ||
+                 ($Activ5 != null && $Activ5->getRequiereInfEscolar()) ||
+                 ($Activ6 != null && $Activ6->getRequiereInfEscolar());
     }
 
     public function getRequiereCamaraBarro()
@@ -109,125 +114,137 @@ trait ConActividades
         $Activ5 = $this->getActividad5();
         $Activ6 = $this->getActividad6();
         
-        return ($Activ1 != null && $Activ1->getRequiereCamaraBarro()) || ($Activ2 != null && $Activ2->getRequiereCamaraBarro()) ||
-             ($Activ3 != null && $Activ3->getRequiereCamaraBarro())|| ($Activ4 != null && $Activ4->getRequiereCamaraBarro()
-             || ($Activ5 != null && $Activ5->getRequiereCamaraBarro())|| ($Activ6 != null && $Activ6->getRequiereCamaraBarro()));
+        return ($Activ1 != null && $Activ1->getRequiereCamaraBarro()) ||
+                 ($Activ2 != null && $Activ2->getRequiereCamaraBarro()) ||
+                 ($Activ3 != null && $Activ3->getRequiereCamaraBarro()) ||
+                 ($Activ4 != null && $Activ4->getRequiereCamaraBarro()) ||
+                 ($Activ5 != null && $Activ5->getRequiereCamaraBarro()) ||
+                 ($Activ6 != null && $Activ6->getRequiereCamaraBarro());
     }
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function getActividad1() {
-		return $this->Actividad1;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function setActividad1($Actividad1) {
-		$this->Actividad1 = $Actividad1;
-		return $this;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function getActividad2() {
-		return $this->Actividad2;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function setActividad2($Actividad2) {
-		$this->Actividad2 = $Actividad2;
-		return $this;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function getActividad3() {
-		return $this->Actividad3;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function setActividad3($Actividad3) {
-		$this->Actividad3 = $Actividad3;
-		return $this;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function getActividad4() {
-		return $this->Actividad4;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function setActividad4($Actividad4) {
-		$this->Actividad4 = $Actividad4;
-		return $this;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function getActividad5() {
-		return $this->Actividad5;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function setActividad5($Actividad5) {
-		$this->Actividad5 = $Actividad5;
-		return $this;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function getActividad6() {
-		return $this->Actividad6;
-	}
-	
-	/**
-	 *
-	 * @ignore
-	 *
-	 */
-	public function setActividad6($Actividad6) {
-		$this->Actividad6 = $Actividad6;
-		return $this;
-	}
-	
 
-   
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function getActividad1()
+    {
+        return $this->Actividad1;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function setActividad1($Actividad1)
+    {
+        $this->Actividad1 = $Actividad1;
+        return $this;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function getActividad2()
+    {
+        return $this->Actividad2;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function setActividad2($Actividad2)
+    {
+        $this->Actividad2 = $Actividad2;
+        return $this;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function getActividad3()
+    {
+        return $this->Actividad3;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function setActividad3($Actividad3)
+    {
+        $this->Actividad3 = $Actividad3;
+        return $this;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function getActividad4()
+    {
+        return $this->Actividad4;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function setActividad4($Actividad4)
+    {
+        $this->Actividad4 = $Actividad4;
+        return $this;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function getActividad5()
+    {
+        return $this->Actividad5;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function setActividad5($Actividad5)
+    {
+        $this->Actividad5 = $Actividad5;
+        return $this;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function getActividad6()
+    {
+        return $this->Actividad6;
+    }
+
+    /**
+     *
+     * @ignore
+     *
+     */
+    public function setActividad6($Actividad6)
+    {
+        $this->Actividad6 = $Actividad6;
+        return $this;
+    }
 }
