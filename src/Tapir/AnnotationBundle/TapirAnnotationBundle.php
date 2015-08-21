7152098547493d1854b078e3e712f5f62f2c2a77
Alejandro Diaz <alediaz.rc@gmail.com>
2015-08-21 16:58:58 +0000

    Former-commit-id: c5301d524b8e35bff63b39ebfa8f375c9420c02c

--- src/Tapir/AnnotationBundle/TapirAnnotationBundle.php
+++ src/Tapir/AnnotationBundle/TapirAnnotationBundle.php
@@ -1,18 +1,9 @@
 <?php
+
 namespace Tapir\AnnotationBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Doctrine\Common\Annotations\AnnotationRegistry;
 
 class TapirAnnotationBundle extends Bundle
 {
-
-    public function boot()
-    {
-        $kernel = $this->container->get('kernel');
-        
-        AnnotationRegistry::registerFile($kernel->locateResource('@TapirAnnotationBundle/Annotation/Descripcion.php'));
-        AnnotationRegistry::registerFile(
-            $kernel->locateResource('@TapirAnnotationBundle/Annotation/AnnotationPrueba.php'));
-    }
 }
