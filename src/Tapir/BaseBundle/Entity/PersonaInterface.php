82ef9c541498f66888e4b8b29735b70e84be5a36
Ernesto Carrea <equistango@gmail.com>
2015-08-27 14:40:08 +0000

    Former-commit-id: 6062d3d0da5fdb4dd4636c497de7bb23881e234a

--- src/Tapir/BaseBundle/Entity/PersonaInterface.php
+++ src/Tapir/BaseBundle/Entity/PersonaInterface.php
@@ -1,6 +1,13 @@
 <?php
 namespace Tapir\BaseBundle\Entity;
 
+/**
+ * Interfaz para que los bundles que implementen la persona (usuario).
+ *
+ * @see http://symfony.com/doc/current/cookbook/doctrine/resolve_target_entity.html
+ *
+ * @author Ernesto Carrea <ernestocarrea@gmail.com>
+ */
 interface PersonaInterface
 {
     /**
