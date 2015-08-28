83d0d073c98da2b37b97e1dde5c86c82fe8cfdeb
Ernesto Carrea <equistango@gmail.com>
2015-08-28 14:09:07 +0000

    Former-commit-id: d4bed19861c747b5cee5446515f14dff7fc7af1e

--- tests/Tapir/BaseBundle/Helper/CuiltTest.php
+++ tests/Tapir/BaseBundle/Helper/CuiltTest.php
@@ -8,7 +8,7 @@ class CuiltTest extends PruebaUnitaria
 
     public function testEsCuiltValida()
     {
-        $this->assertTrue(Cuilt::EsCuiltValida('20-25248532-6'), 'La CUIT 20-25248532-7 debería ser válida.');
+        $this->assertTrue(Cuilt::EsCuiltValida('20-25248532-6'), 'La CUIT 20-25248532-6 debería ser válida.');
         $this->assertFalse(Cuilt::EsCuiltValida('20-25248532-7'), 'La CUIT 20-25248532-7 no debería ser válida.');
     }
 
