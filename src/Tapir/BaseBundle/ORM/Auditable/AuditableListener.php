f15559cd095172b17093cc52ca6fcfdd0ee2391e
Ernesto Carrea <equistango@gmail.com>
2015-09-03 14:41:58 +0000

    Former-commit-id: a51340f0bf8a78b8bb32fa6fc25de575af6cbb0a

--- src/Tapir/BaseBundle/ORM/Auditable/AuditableListener.php
+++ src/Tapir/BaseBundle/ORM/Auditable/AuditableListener.php
@@ -1,7 +1,11 @@
 <?php
 namespace Tapir\BaseBundle\ORM\Auditable;
 
-use Doctrine\ORM\Event\LifecycleEventArgs, Doctrine\Common\EventSubscriber, Doctrine\ORM\Event\OnFlushEventArgs, Doctrine\ORM\Events;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\Common\EventArgs;
+use Doctrine\ORM\Events;
 use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 
 /**
@@ -39,7 +43,6 @@ class AuditableListener implements EventSubscriber
         return $this->logChangeSet($eventArgs, 'editar');
     }
 
-
     /**
      * Interviene la eliminación de una entidad para generar un registro de auditoría.
      *
@@ -53,17 +56,22 @@ class AuditableListener implements EventSubscriber
         $user = $this->container->get('security.context')->getToken()->getUser();
 
         if ($this->isEntitySupported($classMetadata->reflClass)) {
-            $Registro = new \Tapir\BaseBundle\Entity\AuditoriaRegistro();
-            $Registro->setAccion('eliminar');
-            $Registro->setElementoTipo($classMetadata->reflClass->getName());
-            $Registro->setElementoId($entity->getId());
-            $Registro->setEstacion($this->container->get('request')->getClientIp());
-            $Registro->setUsuario($user->getId());
-            $em->persist($Registro);
-            $em->flush();
+            /*
+             * $Registro = new \Tapir\BaseBundle\Entity\AuditoriaRegistro();
+             * $Registro->setAccion('eliminar');
+             * $Registro->setElementoTipo($classMetadata->reflClass->getName());
+             * $Registro->setElementoId($entity->getId());
+             * $Registro->setEstacion($this->container->get('request')->getClientIp());
+             * if (\Tapir\BaseBundle\Helper\ClassHelper::UsaTrait($user, 'Tapir\BaseBundle\Entity\ConIdMetodos')) {
+             * $Registro->setUsuario($user->getId());
+             * }
+             * $em->persist($Registro);
+             * $em->flush();
+             */
+
+            $this->WriteToLog('eliminar', $entity, $user);
         }
     }
-
 
     /**
      * Genera un registro de auditoría con un detalle de los cambios realizados a la entidad.
@@ -82,35 +90,61 @@ class AuditableListener implements EventSubscriber
             $uow->computeChangeSet($classMetadata, $entity);
             $changeSet = $uow->getEntityChangeSet($entity);
 
-            $Registro = new \Tapir\BaseBundle\Entity\AuditoriaRegistro();
-            $Registro->setAccion($action);
-            $Registro->setElementoTipo($classMetadata->reflClass->getName());
-            $Registro->setElementoId($entity->getId());
-            $Registro->setEstacion($this->container->get('request')->getClientIp());
-            if(\Tapir\BaseBundle\Helper\ClassHelper::UsaTrait($user, 'Tapir\BaseBundle\Entity\ConIdMetodos')) {
-                // Algunas veces el usuario no tiene ID (por ejemplo en el entorno de pruebas unitarias)
-                $Registro->setUsuario($user->getId());
-            }
-            $Registro->setCambios(json_encode($changeSet));
-            $em->persist($Registro);
-            $em->flush();
+            $this->WriteToLog($action, $entity, $user, $changeSet);
+
+            /*
+             * $Registro = new \Tapir\BaseBundle\Entity\AuditoriaRegistro();
+             * $Registro->setAccion($action);
+             * $Registro->setElementoTipo($classMetadata->reflClass->getName());
+             * $Registro->setElementoId($entity->getId());
+             * $Registro->setEstacion($this->container->get('request')->getClientIp());
+             * if (\Tapir\BaseBundle\Helper\ClassHelper::UsaTrait($user, 'Tapir\BaseBundle\Entity\ConIdMetodos')) {
+             * // Algunas veces el usuario no tiene ID (por ejemplo en el entorno de pruebas unitarias)
+             * $Registro->setUsuario($user->getId());
+             * }
+             * // echo '<pre>' . json_encode($changeSet, JSON_PRETTY_PRINT) . '</pre>';
+             * $Registro->setCambios(json_encode($changeSet));
+             * $em->persist($Registro);
+             * //$em->flush();
+             * //$em->clear();
+             * //$RegistroMeta = $em->getClassMetadata(get_class($Registro));
+             * //$uow->computeChangeSet($RegistroMeta, $Registro);
+             */
         }
+    }
+
+    /**
+     * Escribe un evento en el log.
+     */
+    protected function WriteToLog($action, $entity, $user, $changeSet = null)
+    {
+        if (\Tapir\BaseBundle\Helper\ClassHelper::UsaTrait($user, 'Tapir\BaseBundle\Entity\ConIdMetodos')) {
+            $logUser = $user->getId();
+        } else {
+            $logUser = (string) $user;
+        }
+
+        $log = $this->container->get('audit.logger');
+        $log->addInfo(
+            $action . ' ' . get_class($entity) . ' ' . $entity->getId() . ' ' . $logUser . ' ' .
+                 json_encode($changeSet, JSON_PRETTY_PRINT));
     }
 
     /**
      * Devuelve true si la clase es auditable.
      *
      * @param ReflectionClass $reflClass
-     * @return boolean
+     * @return boolean True si la clase es auditable.
      */
     protected function isEntitySupported(\ReflectionClass $reflClass)
     {
-        return \Tapir\BaseBundle\Helper\ClassHelper::UsaTrait($reflClass->getName(), 'Tapir\BaseBundle\Entity\Auditable');
+        return \Tapir\BaseBundle\Helper\ClassHelper::UsaTrait($reflClass->getName(),
+            'Tapir\BaseBundle\Entity\Auditable');
     }
-
 
     public function getSubscribedEvents()
     {
+        // $events = [Events::onFlush];
         $events = [Events::postPersist, Events::postUpdate, Events::preRemove];
 
         return $events;
