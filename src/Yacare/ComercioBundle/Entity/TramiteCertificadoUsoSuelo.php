bb0ccaf5d29eb7fcf1f1efedbcab3dcd927ede9b
Ernesto Carrea <equistango@gmail.com>
2015-09-14 14:40:28 +0000

    Former-commit-id: c215938acbd80bf7af37a513a7c49a95111afb68

--- src/Yacare/ComercioBundle/Entity/TramiteCertificadoUsoSuelo.php
+++ src/Yacare/ComercioBundle/Entity/TramiteCertificadoUsoSuelo.php
@@ -6,7 +6,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Representa un Certificado de factibilidad de uso de suelo.
- * 
+ *
  * @author Alejandro Díaz <alediaz.rc@gmail.com>
  *
  * @ORM\Entity(repositoryClass="Tapir\BaseBundle\Entity\TapirBaseRepository")
@@ -16,27 +16,27 @@ class TramiteCertificadoUsoSuelo
 {
     use \Tapir\BaseBundle\Entity\ConId;
     use \Tapir\BaseBundle\Entity\ConNombre;
-    use \Tapir\BaseBundle\Entity\ConFechaValidezHasta;
     use \Tapir\BaseBundle\Entity\Versionable;
+    use \Yacare\BaseBundle\Entity\ConFechaValidezHasta;
     use \Yacare\AdministracionBundle\Entity\ConActoAdministrativo;
     use \Yacare\ComercioBundle\Entity\ConActividades;
     use \Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
-    
+
     /**
      * Indica la fecha en la que se emitio el certificado.
-     * 
+     *
      * @var \Date
-     * 
+     *
      * @ORM\Column(type="date", nullable=false)
      * @Assert\Type("\Date")
      */
     private $FechaEmision;
-    
+
     /**
      * Indica el codigo de resolución condicionada.
-     * 
+     *
      * @var string
-     * 
+     *
      * @ORM\Column(type="string", nullable=true)
      */
     private $ResolucionCondicionada;
